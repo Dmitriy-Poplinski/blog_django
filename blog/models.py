d0b39976033fda7e1b165a58ba2974d865514746
Poplinskiy Dmytro <super.shepard2@gmail.com>
2024-02-23 17:07:51 +0200

    feat: added PublishedManager to Model

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,14 @@ from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (super()
+                .get_queryset()
+                .filter(status=Post.Status.PUBLISHED)
+                )
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = "DF", "Drafted"
@@ -24,6 +32,9 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ["-publish"]
