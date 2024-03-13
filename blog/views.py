7cc65be98b86fde0766d161a980988a3e3267b22
Poplinskiy Dmytro <super.shepard2@gmail.com>
2024-03-13 23:17:52 +0200

    feat: added count of comments to post_detail

--- blog/views.py
+++ blog/views.py
@@ -41,8 +41,17 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
-    return render(request, "blog/post/detail.html", {"post": post})
+    return render(
+        request,
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+        })
 
 
 def post_share(request, post_id):
