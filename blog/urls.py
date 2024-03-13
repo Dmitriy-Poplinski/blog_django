f64d4e8a2af4a50c2242c0b249a6b10ce20d08d6
Poplinskiy Dmytro <super.shepard2@gmail.com>
2024-03-13 15:15:02 +0200

    refactor: change post_list to Class based view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail, PostListView
 
 app_name = "blog"
 
 urlpatterns = [
-    path("", post_list, name="post-list"),
+    # path("", post_list, name="post-list"),
+    path("", PostListView.as_view(), name="post-list"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>", post_detail, name="post-detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,16 @@ from django.core.paginator import (
     EmptyPage,
     PageNotAnInteger,
 )
+from django.views.generic import ListView
 
 from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_list(request):
