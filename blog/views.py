c7996ccef2182006b6751ab3570d731e4106cd64
Poplinskiy Dmytro <super.shepard2@gmail.com>
2024-03-13 16:41:43 +0200

    feat: added post-share template and modified post_share view for sending email

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import (
     PageNotAnInteger,
 )
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
 from .forms import EmailPostForm
@@ -50,14 +51,27 @@ def post_share(request, post_id):
         status=Post.Status.PUBLISHED
     )
 
+    sent = False
+
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (f"{cd['name']} reccomend you to read "
+                       f"{post.title}")
+            message = (f"Read {post.title} at {post_url}\n\n"
+                       f"{cd['name']}'s comments: {cd['comments']}")
+            # TODO Need to configure SMTP Server
+            send_mail(subject, message, "super.shepard2@gmail.com", [cd["to"]])
+            sent = True
     else:
         form = EmailPostForm()
 
     return render(request, "blog/post/share.html", {
         "post": post,
         "form": form,
+        "sent": sent,
     })
